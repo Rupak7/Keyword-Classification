8e78836144e34638717319cea0619a282b981010
Rupak.joko7 <rupakkalita3@gmail.com>
2019-02-16 00:22:32 +0530

    Update keyword_extract.py

--- keyword_extract.py
+++ keyword_extract.py
@@ -4,12 +4,13 @@ import pdftotext
 import pandas as pd
 import numpy as np
 import sys
+from nltk.stem import PorterStemmer
 
 #nltk.download()
-
+ps = PorterStemmer()
 #coprora = body of text
 #lexicon =  a dictionary i.e words and their means
-
+#steming = find the root word
 
 text = "The question of metrics, should be considered early, but this would also require a reference set of input item: a training set of sort, even though we are working off a pre-defined dictionary category-keywords (typically training sets are used to determine this very list of category-keywords, along with a weight factor). Of course such reference/training set should be both statistically significant and statistically representative [of the whole set]."
 
@@ -23,4 +24,8 @@ for i in words:
     if i not in stop_words:
         filtered_text.append(i)
 
-print filtered_text
+filter_txt_stem=[]
+for i in filtered_text:
+    filter_txt_stem.append(ps.stem(i))
+
+print filter_txt_stem
