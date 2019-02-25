543ced221a1192192133b83c85780a8c40788e63
Rupak.joko7 <rupakkalita3@gmail.com>
2019-02-25 19:08:57 +0530

    Update keyword_extract.py

--- keyword_extract.py
+++ keyword_extract.py
@@ -1,9 +1,7 @@
-import pdftotext
 import pandas as pd
 import numpy as np
 import sys
 from collections import Counter
-import argparse
 import string
 from tqdm import tqdm
 
@@ -12,16 +10,6 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer, PorterStemmer, SnowballStemmer
 from nltk.tag import pos_tag
 
-def read_pdf(pdf_file):
-    with open(pdf_file, 'rb') as file_:
-        pdf = pdftotext.PDF(file_)
-
-    text = []
-
-    for i in range(len(pdf)):
-        text.append(pdf[i].replace('  ', ''))
-
-    return text
 
 def remove_stopwords(words):
     for stopword in stop_words:
@@ -35,82 +23,68 @@ def calculate_tf_idf(raw_count, max_raw_count_in_document, no_documents, no_docu
     idf = np.log(no_documents/(1 + no_documents_in_which_word_occured))
     return tf*idf
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--pdf_file", help="If a PDF File, the path to that file", type=str)
-parser.add_argument("--text_file", help="If a TEXT file, the patht to that file", type=str)
-parser.add_argument("--save_to", help="Path to CSV in to write/store Generated Keywords", default="keywords.csv", type=str)
-
-args = parser.parse_args()
-
-document_text = []
-
-if args.pdf_file:
-    document_text = read_pdf(args.pdf_file)
-elif args.text_file:
-    with open(args.text_file, 'r') as f:
-        document_text.append(f.read())
-else:
-    print("ERROR: No File Given to read.")
-    sys.exit(1)
-
-save_to = args.save_to
-
 stop_words = set(stopwords.words("english"))
-stemmer = SnowballStemmer('english')
-tokenizer = RegexpTokenizer('[A-Z][a-z]\w+')
 
-print("Generating Keywords ..")
+def userkeyword(user_input):
+    document_text = list(user_input.split(" "))
+    stemmer = SnowballStemmer('english')
+    tokenizer = RegexpTokenizer('[A-Z][a-z]\w+')
 
-# Tokenizing and collecting all NNPs (Proper Nouns)
-for i in tqdm(range(len(document_text)), desc='Tokenizing: '):
-    document_text[i] = pos_tag(word_tokenize(document_text[i]))
-    document_text[i] = [word[0] for word in document_text[i] if word[1].startswith("VB") or word[1].startswith("NN") or word[1].startswith("JJ")]
-    document_text[i] = [word.lower() for word in document_text[i]]
+    print("Generating Keywords ..")
 
-# Remove all Stop words
-for i in tqdm(range(len(document_text)), desc='Cleaning: '):
-    document_text[i] = remove_stopwords(document_text[i])
+    # Tokenizing and collecting all NNPs (Proper Nouns)
+    for i in tqdm(range(len(document_text)), desc='Tokenizing: '):
+        document_text[i] = pos_tag(word_tokenize(document_text[i]))
+        document_text[i] = [word[0] for word in document_text[i] if word[1].startswith("VB") or word[1].startswith("NN") or word[1].startswith("JJ")]
+        document_text[i] = [word.lower() for word in document_text[i]]
 
-# Lemmatizing
-#for i in range(len(document_text)):
-#    document_text[i] = [lemmatizer.lemmatize(word) for word in document_text[i]]
-for i in tqdm(range(len(document_text)), desc='Stemming: '):
-    document_text[i] = [stemmer.stem(word) for word in document_text[i]]
+    # Remove all Stop words
+    for i in tqdm(range(len(document_text)), desc='Cleaning: '):
+        document_text[i] = remove_stopwords(document_text[i])
 
-total_text = []
+    # Lemmatizing
+    #for i in range(len(document_text)):
+    #    document_text[i] = [lemmatizer.lemmatize(word) for word in document_text[i]]
+    for i in tqdm(range(len(document_text)), desc='Stemming: '):
+        document_text[i] = [stemmer.stem(word) for word in document_text[i]]
 
-for text in document_text:
-    total_text += list(text)
+    total_text = []
 
-# Raw Count for all keywords
-word_count = Counter(total_text)
-raw_count = Counter(total_text)
-max_raw_count_in_document = next(iter(word_count.values()))
-
-# Calculating TF-IDF Values for all keywords
-for word in tqdm(word_count, desc='Calculating TF-IDF: '):
-    count = 0
     for text in document_text:
-        if word in text:
-            count += 1
+        total_text += list(text)
 
-    no_documents_in_which_word_occured = count
-    word_count[word] = calculate_tf_idf(word_count[word], max_raw_count_in_document, len(document_text), no_documents_in_which_word_occured)
+    # Raw Count for all keywords
+    word_count = Counter(total_text)
+    raw_count = Counter(total_text)
+    max_raw_count_in_document = next(iter(word_count.values()))
 
-word_count = list(word_count.items())
+    # Calculating TF-IDF Values for all keywords
+    for word in tqdm(word_count, desc='Calculating TF-IDF: '):
+        count = 0
+        for text in document_text:
+            if word in text:
+                count += 1
 
-i = 0
-for w in raw_count:
-    word_count[i] = word_count[i] + (raw_count[w], )
-    i += 1
+        no_documents_in_which_word_occured = count
+        word_count[word] = calculate_tf_idf(word_count[word], max_raw_count_in_document, len(document_text), no_documents_in_which_word_occured)
 
-# Storing all the keywords in Data-Frame
-df = pd.DataFrame(list(word_count), columns=['Keywords', 'TF_IDF', "Raw_Count"])
+    word_count = list(word_count.items())
 
-df = df.sort_values('TF_IDF', ascending=True)
-df.to_csv(save_to)
+    i = 0
+    for w in raw_count:
+        word_count[i] = word_count[i] + (raw_count[w], )
+        i += 1
 
-print(df.head(10))
+    # Storing all the keywords in Data-Frame
+    df = pd.DataFrame(list(word_count), columns=['Keywords', 'TF_IDF', "Raw_Count"])
 
-print("Keywords saved to {}".format(save_to))
+    df = df.sort_values('TF_IDF', ascending=True)
+
+    cleaned_input = []
+    for i in df.Keywords:
+        cleaned_input.append(i)
+
+    str1 = ' '.join(cleaned_input)
+    return str1
+
+print (userkeyword("I have stomach pain"))
